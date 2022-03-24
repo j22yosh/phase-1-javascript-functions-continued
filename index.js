5ee523b1dba64998bfb202bde2431ed7767a055c
Joshua <j22yosh@gmail.com>
2022-03-24 14:34:29 -0400

    commit tree 103900a6dcd8d4e3de388062f7178dcd5df79ae6

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+function saturdayFun(string){
+if(string === undefined){
+    return "This Saturday, I want to roller-skate!"
+    
+}
+else return "This Saturday, I want to " + `${string}` + "!"
+}
+
+function mondayWork(string){
+    if(string === undefined){
+        return "This Monday, I will go to the office."
+        
+    }
+    else return "This Monday, I will " + `${string}` + "."
+    }
+
+    function wrapAdjective(name){
+        if (name === undefined){
+            return "*"
+        }
+
+        return function (string){
+            if(string === undefined){
+                return "special"}
+                else return "You are " + `${name}` + `${string}` + `${name}` + "!"
+            }
+    }
